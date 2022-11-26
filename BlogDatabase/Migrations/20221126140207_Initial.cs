25575f58000cf2251279acb8b639e06d80486fd6
sabihoshi <sabihoshi.dev@gmail.com>
2022-11-26 22:08:07 +0800

    feat: Initial Project

---
+++ BlogDatabase/Migrations/20221126140207_Initial.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlogDatabase.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    SongId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.SongId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArtistSong",
+                columns: table => new
+                {
+                    ArtistsArtistId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SongsSongId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArtistSong", x => new { x.ArtistsArtistId, x.SongsSongId });
+                    table.ForeignKey(
+                        name: "FK_ArtistSong_Artists_ArtistsArtistId",
+                        column: x => x.ArtistsArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArtistSong_Songs_SongsSongId",
+                        column: x => x.SongsSongId,
+                        principalTable: "Songs",
+                        principalColumn: "SongId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArtistSong_SongsSongId",
+                table: "ArtistSong",
+                column: "SongsSongId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArtistSong");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Songs");
+        }
+    }
+}
---
+++ BlogDatabase/Model.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class BloggingContext : DbContext
+{
+    public BloggingContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "blogging.db");
+    }
+
+    public DbSet<Artist> Artists { get; set; }
+
+    public DbSet<Song> Songs { get; set; }
+
+    public string DbPath { get; }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite($"Data Source={DbPath}");
+}
+
+public class Song
+{
+    public int SongId { get; set; }
+
+    public List<Artist> Artists { get; set; }
+
+    public string Name { get; set; }
+}
+
+public class Artist
+{
+    public int ArtistId { get; set; }
+
+    public List<Song> Songs { get; set; }
+
+    public string Name { get; set; }
+}
